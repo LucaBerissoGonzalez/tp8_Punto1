67eeb7b60238a9d6192cbb63b4045f509d2ba8bc
lucaberissogonzalez <lucaberisso1@gmail.com>
2022-11-11 11:09:35 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tp8_Punto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
